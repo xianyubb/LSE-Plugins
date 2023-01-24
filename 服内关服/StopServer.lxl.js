575c8cd18ffb352344d0707e6ab85bc6d923741e
xianyubb <2149656630@qq.com>
2023-01-24 18:32:00 +0800

    1

---
+++ 服内关服/StopServer.lxl.js
@@ -0,0 +1,46 @@
+//LiteLoaderScript Dev Helper
+/// <reference path="c:\Users\Administrator\.vscode/dts/HelperLib-master/src/index.d.ts"/>
+
+ll.registerPlugin(
+  /* name */ "StopServer",
+  /* introduction */ "更加易用的线上表单关服插件",
+  /* version */ [0, 0, 1],
+  /* otherInformation */ {}
+);
+
+//插件加载成功提示
+log("StopServer加载成功")
+
+
+//注册命令stops
+mc.listen("onServerStarted", () => {
+  const cmd = mc.newCommand("stops", "打开表单", PermType.Any, 0x80);
+  cmd.optional("action", ParamType.Bool);
+  cmd.overload(["action"]);
+  cmd.setCallback((_cmd, _ori, our, res) => {});
+  cmd.setup();
+});
+
+//监听玩家使用stops指令并打开表单
+mc.listen("onPlayerCmd", (pl, cmd) => {
+  if (cmd === "stops") {
+    pl.sendModalForm(
+      "线上关服[提示]",
+      "§4真的要关闭服务器吗？",
+      "确定",
+      "放弃",
+      function (pl, res) {
+        if (res === true) {
+          mc.broadcast("[§4§lWARNING§r]§6服务器将在10秒后关闭");
+          setTimeout(() => {
+            pl.tell("§4§l服务器已关闭");
+            mc.runcmd("stop");
+          }, 10000);
+        } else {
+          pl.tell("[§5§l提示§r]§9您已取消关闭服务器");
+        }
+      }
+    );
+  }
+});
+          
