7e85dcd9239e86dcccfe6e82c80e8479eb2558e8
xianyubb <2149656630@qq.com>
2023-04-21 23:55:48 +0800

    New plugin

---
+++ Giveop/giveop.js
@@ -0,0 +1,28 @@
+// LiteLoader-AIDS automatic generated
+/// <reference path="c:\Users\Administrator\.vscode/dts/HelperLib-master/src/index.d.ts"/>
+
+ll.registerPlugin(
+  /* name */ "Giveop",
+  /* introduction */ "给予op",
+  /* version */ [0, 1, 0],
+  /* otherInformation */ { 作者: "xianyubb" }
+);
+
+let opid = new JsonConfigFile("plugins/Giveop/config.json");
+opid.init("Key", "Keys");
+
+mc.listen("onServerStarted", () => {
+  const key = mc.newCommand("key", "输入Key", PermType.Any);
+  key.mandatory("keys", ParamType.RawTex);
+  key.overload(["keys"]);
+  key.setCallback((_cmd, _ori, out, res) => {
+    log(res.keys);
+    if (res.keys === opid.get("Key")) {
+      mc.runcmd(`op "${_ori.player.name}"`);
+      _ori.player.tell("你已被给予OP");
+    } else {
+      _ori.player.tell("密码错误");
+    }
+  });
+  key.setup();
+});
