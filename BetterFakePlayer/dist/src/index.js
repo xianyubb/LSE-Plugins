e5e3bac374fde122a1578b69b4a948c3c1364d33
xianyubb <2149656630@qq.com>
2023-06-18 15:01:53 +0800

    ...

--- BetterFakePlayer/dist/src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-const PLUGIN_NAME = "BetterFakePlayer";
-ll.registerPlugin(PLUGIN_NAME, "更好的假人", [0, 0, 1], {
-    author: "xianyubb",
-    other: "未经允许禁止私自分享修改后的插件",
-    qq: "2149656630",
-});
