7c4d17be96c2fafb33e8b29fbd7aa4e50c09e9d9
xianyubb <2149656630@qq.com>
2023-06-23 12:24:22 +0800

    '

--- 跨服传送/transfer.llse(1).js
+++ 跨服传送/transfer.llse(1).js
@@ -2,10 +2,11 @@
 /// <reference path="c:\Users\Administrator\.vscode/dts/HelperLib-master/src/index.d.ts"/>
 
 const PLUGIN_NAME = "Transfer";
-const PLUGIN_VERSION = [0, 2, 0];
+const PLUGIN_VERSION = [0, 3, 0];
 
 let _Config = {
   setting: {
+    "MotdIP:Port": "win.xianyubb.top:23005",
     buttons: [
       {
         text: "servername",
@@ -15,34 +16,100 @@ let _Config = {
     ],
   },
 };
+
+/**
+ * @typedef {string} _status 服务器运行状态
+ * @typedef {number} online 服务器在线人数
+ * @typedef {number} max 服务器最大人数
+ * @typedef {string} motd 服务器motd
+ * @typedef {string} level_name 服务器存档名称
+ */
+let _status;
+let online;
+let max;
+let motd;
+let level_name;
+
+
+
 const Config = new JsonConfigFile(
   `.\\plugins\\${PLUGIN_NAME}\\config.json`,
   JSON.stringify(_Config)
 );
-
-let FormJson = {
-  type: "form",
-  title: PLUGIN_NAME,
-  content: "请选择要传送的服务器",
-};
+let setting = Config.get("setting");
+let FormJson = Object.assign(
+  {},
+  {
+    type: "form",
+    title: PLUGIN_NAME,
+    content: "请选择要传送的服务器...",
+  },
+  setting
+);
 
 const transfer = mc.newCommand("transfer", "跨服传送", PermType.Any);
 transfer.overload([]);
 transfer.setCallback((_cmd, _ori, out, res) => {
   if (_ori.player) {
-    _ori.player.sendCustomForm(
-      JSON.stringify(Object.assign({}, FormJson, Config.get("setting"))),
-      (Player, data) => {
-        if (typeof data === "number") {
-          let main = Config.get("setting")["buttons"][data];
-          let IP = main["IP"];
-          let Port = main["Port"];
-          Player.transServer(IP, Port);
-        } else {
-          Player.tell("关闭了表单");
-        }
-      }
-    );
+    sendCustomForm(_ori.player);
   }
 });
 transfer.setup();
+
+/**
+ * 向玩家发送自定义表单（Json格式）
+ * @param {Player} Player
+ */
+function sendCustomForm(Player) {
+  Player.sendCustomForm(JSON.stringify(FormJson), (Player, data) => {
+    if (typeof data === "number") {
+      ServerINFO(Player, data);
+    }
+  });
+}
+/**
+ * 服务器motd查询
+ * @param {Player} Player 玩家
+ * @param {number} id 按钮id
+ */
+function ServerINFO(Player, id) {
+  let main = setting["buttons"][id];
+  let IP = main["IP"];
+  let Port = main["Port"];
+  let url = `http://${setting["MotdIP:Port"]}/api?host=${IP}:${Port}`;
+  network.httpGet(url, (sta, res) => {
+    let result = JSON.parse(res);
+    _status = result["status"];
+    online = result["online"];
+    max = result["max"];
+    motd = result["motd"];
+    level_name = result["level_name"];
+    let content =
+      " 服务器名:  " +
+      main["text"] +
+      "\n 状态:  " +
+      _status +
+      "\n 在线人数/最大人数:  " +
+      online +
+      "/" +
+      max +
+      "\n motd:  " +
+      motd +
+      "\n 存档名：  " +
+      level_name;
+    Player.sendModalForm(
+      PLUGIN_NAME,
+      content,
+      "前往",
+      "返回",
+      (Player, result) => {
+        switch (result) {
+          case true:
+            Player.transServer(IP, Port);
+          case false:
+            sendCustomForm(Player);
+        }
+      }
+    );
+  });
+}
